49f1f40f99f2be2e743545172d65c1883bf87bb0
Rutik  Thorat <rutwikbthorat@gmail.com>
2023-04-23 14:57:49 +0530

    Initial Commit

---
+++ BackWardDataUsingSeparateContainerTwentyFivePractical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackWardDataUsingSeparateContainerTwentyFivePractical
+//
+//  Created by Mac on 23/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
